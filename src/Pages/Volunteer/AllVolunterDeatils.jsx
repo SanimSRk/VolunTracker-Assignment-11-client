24df51f8e2827f7fedf9283f18864e348b3ae83d
SanimSRk <hasansanim562@gmail.com>
2024-05-12 00:40:50 +0600

    be volunteer section desgin is done

--- src/Pages/Volunteer/AllVolunterDeatils.jsx
+++ src/Pages/Volunteer/AllVolunterDeatils.jsx
@@ -1,8 +1,11 @@
+import { useContext } from 'react';
 import { BsInstagram, BsTwitterX } from 'react-icons/bs';
 import { FaFacebook } from 'react-icons/fa6';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../AuthContext/AuthProvider';
 
 const AllVolunterDeatils = () => {
+  const { user } = useContext(AuthContext);
   const usloader = useLoaderData();
   const {
     thumbnail,
@@ -11,6 +14,8 @@ const AllVolunterDeatils = () => {
     category,
     location,
     neededNumber,
+    email,
+    fullName,
     startDate,
   } = usloader;
   return (
@@ -19,7 +24,7 @@ const AllVolunterDeatils = () => {
         <div>
           <img className="w-full" src={thumbnail} alt="" />
         </div>
-        <div className="bg-[#f26837] p-6 text-white">
+        <div className="bg-[#f26837] p-6 text-white ">
           <h2 className="text-xl font-semibold">{title}</h2>
           <div className="flex justify-between mt-4">
             <p className="">{category}</p>
@@ -27,9 +32,51 @@ const AllVolunterDeatils = () => {
           </div>
           <p className="mt-4">Deadline : {startDate}</p>
           <p className="mt-3"> location : {location}</p>
-          <button className="btn text-xl text-[#f26837] font-semibold bg-[#e6e6f2] shadow-lg   w-full mt-12 ">
+          <label
+            htmlFor="my_modal_7"
+            className="btn text-xl text-[#f26837] font-semibold bg-[#e6e6f2] shadow-lg   w-full mt-12 "
+          >
             Be a Volunteer
-          </button>
+          </label>
+          <div className="text-black w-full">
+            {/* The button to open modal */}
+
+            {/* Put this part before </body> tag */}
+            <input type="checkbox" id="my_modal_7" className="modal-toggle" />
+            <div className="modal " role="dialog">
+              <div className="modal-box w-[100vh]">
+                <div className="">
+                  <div className="w-full">
+                    <img className="w-full" src={thumbnail} alt="" />
+                  </div>
+                  <div>
+                    <h2>{title}</h2>
+                    <h2 className="my-3">{category}</h2>
+
+                    <p className="">{description}</p>
+                    <p className="mt-4">location: {location}</p>
+
+                    <p className="my-3">
+                      Need Volunteer : {neededNumber} person
+                    </p>
+                    <p className="">Deadline : {startDate}</p>
+                    <div>
+                      <h2 className="my-3">Organizer name : {fullName}</h2>
+                      <h2>Organizer email : {email}</h2>
+                    </div>
+                  </div>
+                  <h2>Your Name : {user?.displayName}</h2>
+                  <h2>Your email : {user?.email}</h2>
+                </div>
+                <button className="btn w-full mt-4">Request</button>
+              </div>
+
+              <label className="modal-backdrop" htmlFor="my_modal_7">
+                Close
+              </label>
+            </div>
+          </div>
+
           <div className="flex gap-4 text-3xl mt-8 justify-center">
             <FaFacebook></FaFacebook>
             <BsTwitterX></BsTwitterX>
--- src/Pages/Volunteer/Volunteer.jsx
+++ src/Pages/Volunteer/Volunteer.jsx
@@ -4,13 +4,39 @@ import AllVoluntterCart from './AllVoluntterCart';
 
 const Volunteer = () => {
   const [volunteers, setVolunteers] = useState([]);
+
   useEffect(() => {
     axios.get('http://localhost:5000/allVolunteers').then(res => {
       setVolunteers(res.data);
     });
   }, []);
+  const handileClickSubmite = e => {
+    e.preventDefault;
+    const title = e.target.name.value;
+    console.log(title);
+  };
+
   return (
-    <div className="my-[120px]">
+    <div className="mb-[120px] ">
+      <div className="bg-[url(/pexels-shvetsa-5029859.jpg)] lg:h-[580px] md:h-[420px] h-[320px] bg-cover bg-center rounded-lg items-center justify-center grid w-full">
+        <div className="w-full">
+          <div className="input-bordered w-full rounded-lg flex border-2 border-[#f26837] ">
+            <input
+              type="text"
+              name="titles"
+              placeholder="Type here"
+              className=" rounded-r-none lg:w-[400px] input "
+            />
+            <button
+              onSubmit={handileClickSubmite}
+              type="submit"
+              className="btn  text-white bg-[#f26837] rounded-l-none text-xl"
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="lg:w-2/3 text-center mx-auto">
         <h2 className="text-3xl font-bold  ">All Volunteer Need Post</h2>
         <p className="mt-4">
